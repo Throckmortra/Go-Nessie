93be89a121e1d978aa889b9b501ba036c93f9f06
Aaron Throckmorton <rickthrockmorton@gmail.com>
2015-10-01 14:08:31 -0400

    names of elements in JSON structs should be exported. Getters are not needed if fields are exported. Getters are not supported in Golang but can be used in some situations.

--- account.go
+++ account.go
@@ -2,42 +2,12 @@ package gonessie
 
 //Account model from Nessie
 type Account struct {
-	id 				string `json:"id"`
-	accountType 	string `json:"type"`
-	nickname 		string `json:"nickname"`
-	rewards 		int    `json:"rewards"`
-	balance 		int    `json:"balance"`
-	accountNumber	string `json:"account_number"`
-	customerId 		string `json:"customer_id"`
+	ID         string `json:"id"`
+	Nickname   string `json:"nickname"`
+	Rewards    int    `json:"rewards"`
+	Balance    int    `json:"balance"`
+	CustomerID string `json:"customer_id"`
 }
 
 //Accounts array
 type Accounts []Account
-
-func (a Account) GetId() string {
-	return a.id
-}
-
-func (a Account) GetType() string {
-	return a.accountType
-}
-
-func (a Account) GetNickname() string {
-	return a.nickname
-}
-
-func (a Account) GetRewards() string {
-	return a.rewards
-}
-
-func (a Account) GetBalance() int {
-	return a.balance
-}
-
-func (a Account) GetAccountNumber() string {
-	return a.accountNumber
-}
-
-func (a Account) GetCustomerId() string {
-	return a.customerId
-}
